ea5c8d4f41b73d47e61a8f1f8f0cf70b2ed9b6c8
Bach Nguyen <123403526+bachnguyen234@users.noreply.github.com>
2026-08-20 11:32:10 +0700

    Delete export_dashboard.py

--- export_dashboard.py
+++
@@ -1,5 +0,0 @@
-import requests
-# give your server a second to start, or start this after it's already running
-r = requests.get("http://127.0.0.1:8050")
-with open("dashboard_snapshot.html", "w", encoding="utf-8") as f:
-    f.write(r.text)
